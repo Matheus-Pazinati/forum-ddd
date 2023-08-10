f20015003f23a6e6aaa5cf79434633cb75ced23d
Matheus Martins Pazinati <matheuspazinati@gmail.com>
2023-08-10 11:54:35 -0300

    create factories for make automatized entities for tests

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -3,6 +3,7 @@ import { GetQuestionBySlugUseCase } from './get-question-by-slug'
 import { UniqueEntityID } from '@/core/entities/unique-entity.id'
 import { Question } from '../../enterprise/entities/question'
 import { Slug } from '../../enterprise/entities/value-objects/slug'
+import { makeQuestion } from 'test/factories/make-question'
 
 describe('Search a Question by his Slug', () => {
   let inMemoryQuestionsRepository: InMemoryQuestionsRepository
@@ -14,17 +15,14 @@ describe('Search a Question by his Slug', () => {
   })
 
   test('it should be able to get a question by his slug', async () => {
-    const newQuestion = Question.create({
-      authorId: new UniqueEntityID('01'),
-      content: 'Test question',
-      title: 'Test question',
-      slug: Slug.create('test-question'),
+    const newQuestion = makeQuestion({
+      slug: Slug.create('test-slug')
     })
 
     await inMemoryQuestionsRepository.create(newQuestion)
 
     const { question } = await searchBySlug.execute({
-      slug: newQuestion.slug.value,
+      slug: 'test-slug',
     })
 
     expect(question.id).toBeTruthy()
---
+++ test/factories/make-question.ts
@@ -0,0 +1,14 @@
+import { UniqueEntityID } from "@/core/entities/unique-entity.id";
+import { Question, QuestionProps } from "@/domain/forum/enterprise/entities/question";
+import { faker } from "@faker-js/faker"
+
+export function makeQuestion(override: Partial<QuestionProps> = {}, id?: UniqueEntityID) {
+  const question = Question.create({
+    authorId: new UniqueEntityID('01'),
+    content: faker.lorem.text(),
+    title: faker.lorem.sentence(),
+    ...override
+  }, id)
+
+  return question
+}
