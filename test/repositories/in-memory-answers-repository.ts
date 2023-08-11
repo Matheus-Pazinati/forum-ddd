843f4f397606d619c7e413521d57e4e3902065f4
Matheus Martins Pazinati <matheuspazinati@gmail.com>
2023-08-11 19:50:50 -0300

    create use-case delete-answer

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -4,7 +4,29 @@ import { Answer } from "@/domain/forum/enterprise/entities/answer";
 export class InMemoryAnswersRepository implements AnswersRepository {
   public answers: Answer[] = []
 
+  async findById(answerId: string) {
+    const answer = this.answers.find((answer) => {
+      return answer.id.toString() === answerId
+    })
+
+    if (!answer) {
+      return null
+    }
+
+    return answer
+  }
+
   async create(answer: Answer) {
     this.answers.push(answer)
+  }
+
+  async delete(answer: Answer): Promise<void> {
+    const deletedAnswerIndex = this.answers.findIndex((item) => {
+      return item.id === answer.id
+    })
+
+    this.answers.splice(deletedAnswerIndex, 1)
+
+
   }
 }
