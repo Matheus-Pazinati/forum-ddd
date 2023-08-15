a708a44171ed8446d2cec327bbb30ebf2cd04d8c
Matheus Martins Pazinati <matheuspazinati@gmail.com>
2023-08-15 17:24:25 -0300

    validate if question exists before create a answer for her

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,23 +1,43 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 import { AnswerQuestionUseCase } from './answer-question'
 import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { makeQuestion } from 'test/factories/make-question'
 
 let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let createAnswer: AnswerQuestionUseCase
 
 describe('Create Answer for a question', () => {
   beforeEach(() => {
     inMemoryAnswersRepository = new InMemoryAnswersRepository()
-    createAnswer = new AnswerQuestionUseCase(inMemoryAnswersRepository)
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    createAnswer = new AnswerQuestionUseCase(
+      inMemoryAnswersRepository,
+      inMemoryQuestionsRepository
+    )
   })
 
   test('it should be able to answer a question', async () => {
+    const question = makeQuestion()
+    await inMemoryQuestionsRepository.create(question)
+
     const { answer } = await createAnswer.execute({
-      instructorId: '01',
-      questionId: '01',
+      instructorId: 'instructor-01',
+      questionId: question.id.toString(),
       content: 'Test Answer',
     })
 
     expect(answer.content).toEqual('Test Answer')
     expect(inMemoryAnswersRepository.answers.length).toBe(1)
+  })
+
+  test("it should not be able to create a answer for a nonexistent question", async() => {
+    expect(async() => {
+      await createAnswer.execute({
+        instructorId: 'instructor-01',
+        questionId: 'question-01',
+        content: 'Test Answer',
+      })
+    }).rejects.toBeInstanceOf(Error)
   })
 })
--- src/domain/forum/application/use-cases/answer-question.ts
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -1,6 +1,7 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity.id'
 import { Answer } from '../../enterprise/entities/answer'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
+import { QuestionsRepository } from '../repositories/questions-repository'
 
 interface AnswerQuestionUseCaseRequest {
   questionId: string
@@ -13,13 +14,23 @@ interface AnswerQuestionUseCaseResponse {
 }
 
 export class AnswerQuestionUseCase {
-  constructor(private answersRepository: AnswersRepository) {}
+  constructor(
+    private answersRepository: AnswersRepository,
+    private questionsRepository: QuestionsRepository
+    ) {}
 
   async execute({
     content,
     instructorId,
     questionId,
   }: AnswerQuestionUseCaseRequest): Promise<AnswerQuestionUseCaseResponse> {
+
+    const question = await this.questionsRepository.findById(questionId)
+
+    if (!question) {
+      throw new Error("Question not found.")
+    }
+    
     const answer = Answer.create({
       authorId: new UniqueEntityID(instructorId),
       questionId: new UniqueEntityID(questionId),
