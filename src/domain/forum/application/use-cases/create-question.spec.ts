1dbf11f8266aa371b85e08dabbe8a65c240f1f6f
Matheus Martins Pazinati <matheuspazinati@gmail.com>
2023-08-22 19:18:38 -0300

    add attachment entity on questions

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,5 +1,6 @@
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 import { CreateQuestionUseCase } from './create-question'
+import { UniqueEntityID } from '@/core/entities/unique-entity.id'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let createQuestion: CreateQuestionUseCase
@@ -15,9 +16,15 @@ describe('Create a question', () => {
       authorId: '01',
       content: 'How to create tests ?',
       title: 'Create test',
+      attachmentsIds: ["attachment-1", "attachment-2"]
     })
 
     expect(result.isRight()).toBe(true)
     expect(inMemoryQuestionsRepository.questions[0]).toEqual(result.value?.question)
+    expect(inMemoryQuestionsRepository.questions[0].attachments).toHaveLength(2)
+    expect(inMemoryQuestionsRepository.questions[0].attachments).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityID("attachment-1") }),
+      expect.objectContaining({ attachmentId: new UniqueEntityID("attachment-2") }),
+    ])
   })
 })
--- src/domain/forum/application/use-cases/create-question.ts
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -2,11 +2,13 @@ import { UniqueEntityID } from '@/core/entities/unique-entity.id'
 import { Question } from '../../enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/questions-repository'
 import { Either, right } from '@/core/either'
+import { QuestionAttachment } from '../../enterprise/entities/question-attachment'
 
 interface CreateQuestionUseCaseRequest {
   authorId: string
   title: string
   content: string
+  attachmentsIds?: string[]
 }
 
 type CreateQuestionUseCaseResponse  = Either<null, {
@@ -20,12 +22,25 @@ export class CreateQuestionUseCase {
     authorId,
     content,
     title,
+    attachmentsIds
   }: CreateQuestionUseCaseRequest): Promise<CreateQuestionUseCaseResponse> {
     const question = Question.create({
       authorId: new UniqueEntityID(authorId),
       content,
       title,
     })
+
+    if (attachmentsIds) {
+      const attachments = attachmentsIds.map((attachmentId) => {
+        return QuestionAttachment.create({
+          attachmentId: new UniqueEntityID(attachmentId),
+          questionId: question.id
+        })
+      })
+  
+      question.attachments = attachments
+    }
+
 
     await this.questionRepository.create(question)
 
